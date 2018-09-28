6b5ce5ed6af88d992f4d8e91bc0cf03d287ef71f
jorgenhov <jorhov19@hotmail.com>
2018-09-28 13:14:38 +0200

    first commit

---
+++ src/main/java/com/example/polls/PollsApplication.java
@@ -0,0 +1,26 @@
+package com.example.polls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.sql.Time;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+		PollsApplication.class,
+		Jsr310JpaConverters.class
+})
+public class PollsApplication {
+
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollsApplication.class, args);
+	}
+}
